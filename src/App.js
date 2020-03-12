ce3bf318abc02a14b8e273305f7cc7b9a4b21648
Dhruv Bhatnagar <dhruvbhatnagar10@gmail.com>
2020-03-12 13:40:57 +0530

    Set private to false in package JSON

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import './App.css';
 
-const k = 0;
-
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <p>
           <code>BnB coming soon...</code>
-          { console.log(233) }
         </p>
       </header>
     </div>
