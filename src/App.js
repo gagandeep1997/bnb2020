790ec64a7992336d5446716d3838ceab440bd825
Dhruv Bhatnagar <dhruvbhatnagar10@gmail.com>
2020-03-12 13:38:40 +0530

    Setup ESLint and husky pre-commit script

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 
+const k = 0;
+
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <p>
           <code>BnB coming soon...</code>
+          { console.log(233) }
         </p>
       </header>
     </div>
